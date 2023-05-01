d57ea09145b6c87819178a82e1bb1a5eb69d9f17
Helena Ch <ellenkadata@gmail.com>
2023-05-01 23:14:29 +0200

    update

--- Ukol1/swinging_states.py
+++ Ukol1/swinging_states.py
@@ -1,1 +1,4 @@
-print("test")
+import pandas as pd
+presidents=pd.read_csv("presidents.csv")
+# presidents["rank"]=presidents.groupby(["state","year"])["candidatevotes"].rank(method=max,ascending=False)
+print(presidents.head())
