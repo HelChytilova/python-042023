b62062e4eef8e430af862bb56b85edf08ebb7bee
Helena Ch <ellenkadata@gmail.com>
2023-05-01 23:34:21 +0200

    update

--- Ukol1/swinging_states.py
+++ Ukol1/swinging_states.py
@@ -1,4 +1,5 @@
 import pandas as pd
 presidents=pd.read_csv("presidents.csv")
-# presidents["rank"]=presidents.groupby(["state","year"])["candidatevotes"].rank(method=max,ascending=False)
+presidents["rank"]=presidents.groupby(["state","year"])["candidatevotes"].rank(method="max",ascending=False)
+presidents=presidents.loc[:,"candidate":"rank"]
 print(presidents.head())
